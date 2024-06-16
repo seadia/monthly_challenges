381c723f9543df46064d401b2f32bacf3556b44c
Ian Dorell <idorell@yahoo.com>
2024-06-15 19:45:55 -0600

    Adding a correctly functioning docker file to the repository.  Adding challenges module and requirements.txt

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes a day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes a day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes a day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes a day!",
+    "december": "Learn Django for at least 20 minutes every day!"
+}
+
 
 # Create your views here.
+
+
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
+    
+    
