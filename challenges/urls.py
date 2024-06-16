267cf8e389b6c52b9c5177a2388d183d53281e26
Ian Dorell <idorell@yahoo.com>
2024-06-15 22:00:08 -0600

    Removing pyc files from the repository.  Adding a gitignore to ignore pyc and other file types.  Adding in a templates folder and an index and challenge html file.  Adding in a workspace file

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.index, name="index"), # /challenges/
     path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
+
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 
 monthly_challenges = {
@@ -21,15 +22,34 @@ monthly_challenges = {
 # Create your views here.
 
 
+def index(request):
+    months = list(monthly_challenges.keys())
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+
+
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse(
+        "month-challenge", args=[redirect_month])  # /challenge
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month": month
+        })
+ 
     except:
-        return HttpResponseNotFound("This month is not supported!")
-    
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
     
